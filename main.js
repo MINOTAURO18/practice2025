db5ddfce5698a57f40a791dda46b826b7e5d87d5
john alexander muñoz <alexander12345my@gmail.com>
2025-12-23 18:53:37 -0500

    first commit for 2025, I'm back

---
+++ main.js
@@ -0,0 +1,13 @@
+let nombre = "alexander";
+nombre = "jose";
+console.log(nombre);
+
+const edad = 21;
+let operacion = 5 + 10 
+console.log(operacion)
+
+let combi = 10 > 15 ? 'Im so happy with javascrip' : 'javascrip sucks';
+
+console.log(combi)
+
+
